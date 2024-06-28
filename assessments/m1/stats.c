42c9b96dc6d50054934576c78950fbeab1379268
thaituan52 <tuandothai522004@gmail.com>
2024-06-28 08:17:46 +0700

    update .h

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -41,3 +41,18 @@ void main() {
 }
 
 /* Add other Implementation File Code Here */
+//  main() - The main entry point for your program
+//
+//  print_statistics() - A function that prints the statistics of an array including minimum, maximum, mean, and median.
+//
+//  print_array() -  Given an array of data and a length, prints the array to the screen
+//
+//  find_median() - Given an array of data and a length, returns the median value
+//
+//  find_mean() -  Given an array of data and a length, returns the mean
+//
+//  find_maximum() -  Given an array of data and a length, returns the maximum
+//
+//  find_minimum() -  Given an array of data and a length, returns the minimum
+//
+//  sort_array() - Given an array of data and a length, sorts the array from largest to smallest.  (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
--- assessments/m1/stats.h
+++ assessments/m1/stats.h
@@ -35,6 +35,19 @@
  *
  * @return <Add Return Informaiton here>
  */
-
+    //A function that prints the statistics of an array including minimum, maximum, mean, and median.
+    void print_statistics(unsigned char* data, unsigned int size);
+    //Given an array of data and a length, prints the array to the screen
+    void print_array(unsigned char* data, unsigned int size);
+    //Given an array of data and a length, returns the median value
+    float find_median(unsigned char* data, unsigned int size);
+    //Given an array of data and a length, returns the mean
+    int find_mean(unsigned char* data, unsigned int size);
+    //Given an array of data and a length, returns the maximum
+    int find_maximum(unsigned char* data, unsigned int size);
+     //Given an array of data and a length, returns the minimum
+    int find_minimum(unsigned char* data, unsigned int size);
+     //Given an array of data and a length, sorts the array from largest to smallest.  (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+    void sort_array(unsigned char* data, unsigned int size);
 
 #endif /* __STATS_H__ */
